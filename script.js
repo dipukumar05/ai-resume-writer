48b3b3275f1b6fcf6f902c3c61d863bf5988969d
dipukumar05 <kumardipu4704@gmail.com>
2026-03-08 16:41:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ let name=document.getElementById("name").value;
 let education=document.getElementById("education").value;
 let skills=document.getElementById("skills").value;
 
-document.getElementById("rname").innerText=name;
+let resume = `
+Name: ${name}
 
-document.getElementById("contact").innerText=
-"Bihar, India | +91 | email@example.com";
+Education:
+${education}
 
-document.getElementById("summary").innerText=
-"Detail-oriented and reliable Data Entry Operator with strong typing skills and computer knowledge.";
+Skills:
+${skills}
+`;
 
-document.getElementById("reducation").innerText=education;
-
-document.getElementById("rskills").innerText=skills;
+document.getElementById("result").innerText = resume;
 
 }
